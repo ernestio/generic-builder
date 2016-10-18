de4d4d8d6f339a19d7382e49a54424de1d5f25a8
Tom Bevan <tom.bevan@r3labs.io>
2016-10-18 15:14:02 +0100

    generic network

--- main.go
+++ main.go
@@ -26,6 +26,16 @@ func main() {
 	s.ProcessFailedResponse("vpc.create.error", "vpcs.create.error")
 	s.ProcessFailedResponse("vpc.delete.error", "vpcs.delete.error")
 
+	// Configure Networks Hooks
+	s.ProcessRequest("networks.create", "network.create")
+	s.ProcessRequest("networks.delete", "network.delete")
+
+	s.ProcessSuccessResponse("network.create.done", "network.create", "networks.create.done")
+	s.ProcessSuccessResponse("network.delete.done", "network.delete", "networks.delete.done")
+
+	s.ProcessFailedResponse("network.create.error", "networks.create.error")
+	s.ProcessFailedResponse("network.delete.error", "networks.delete.error")
+
 	// Configure ELBs Hooks
 	s.ProcessRequest("elbs.create", "elb.create")
 	s.ProcessRequest("elbs.delete", "elb.delete")
